45e440baa7faaa4fca846cd1712d3e09f40e0c09
alanblee <alanbenlee12@gmail.com>
2020-11-17 08:52:25 -0800

    updating google sheets will update some of the issue fields

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { GoogleSheetsConnector } = require("reshuffle-google-connectors");
 
 (async () => {
   const app = new Reshuffle();
-
+  // Jira Config
   const jira = new JiraConnector(app, {
     host: process.env.JIRA_HOST,
     protocol: process.env.JIRA_PROTOCOL,
@@ -13,7 +13,7 @@ const { GoogleSheetsConnector } = require("reshuffle-google-connectors");
     password: process.env.JIRA_PASSWORD,
     baseURL: process.env.RUNTIME_BASE_URL,
   });
-
+  // Google Sheets Config
   const googleSheets = new GoogleSheetsConnector(app, {
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
@@ -22,24 +22,48 @@ const { GoogleSheetsConnector } = require("reshuffle-google-connectors");
     sheetsId: process.env.GOOGLE_SHEET_ID,
   });
 
-  // const docInfo = await googleSheets.getInfo();
-  // console.log(`Document has ${docInfo.sheetCount} sheets.`);
-
   jira.on({ jiraEvent: "jira:issue_created" }, async (event, app) => {
     const {
       id,
-      fields: {
-        summary,
-        description,
-        status: { name },
-        assignee: { displayName },
-      },
+      fields: { summary, description },
     } = event.issue;
-
+    // status: { name },
+    // assignee: assignee ? { displayName } : "null" ,
     const sheetId = 0;
-    const values = [id, description, summary, name, displayName];
-    const test = await googleSheets.addRow(sheetId, values);
+    const values = [
+      id,
+      summary,
+      description,
+      event.issue.fields.status.name,
+      event.issue.fields.assignee.displayName
+        ? event.issue.fields.assignee.displayName
+        : "unassigned",
+    ];
+    await googleSheets.addRow(sheetId, values);
   });
+
+  const myHandler = async (event, app) => {
+    if (
+      event.worksheetsChanged[0] &&
+      event.worksheetsChanged[0].rowsChanged[0]
+    ) {
+      const { curr, prev } = event.worksheetsChanged[0].rowsChanged[0];
+      let updates = { fields: {} };
+      for (let key in prev) {
+        if (prev[key] !== curr[key]) {
+          updates = {
+            fields: { ...updates.fields, [[key.toLowerCase()]]: curr[key] },
+          };
+        }
+      }
+      // jira update
+      const updateIssue = await jira
+        .sdk()
+        .updateIssue(curr["Issue ID"], updates);
+    }
+  };
+
+  googleSheets.on({}, myHandler);
 
   app.start(8000);
 })().catch(console.error);
