cc45c4fdd89a2d913cfa694da57dd47967599a31
alanblee <alanbenlee12@gmail.com>
2020-11-16 16:27:03 -0800

    connected google sheets

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Reshuffle } = require("reshuffle");
 const { JiraConnector } = require("reshuffle-jira-connector");
+const { GoogleSheetsConnector } = require("reshuffle-google-connectors");
 
 (async () => {
   const app = new Reshuffle();
@@ -13,10 +14,21 @@ const { JiraConnector } = require("reshuffle-jira-connector");
     baseURL: process.env.RUNTIME_BASE_URL,
   });
 
-  jira.on({ jiraEvent: "jira:issue_created" }, (event, app) => {
-    console.log(event.webhookEvent);
-    console.log(event.issue);
+  const googleSheets = new GoogleSheetsConnector(app, {
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    },
+    sheetsId: process.env.GOOGLE_SHEET_ID,
   });
+
+  const docInfo = await googleSheets.getInfo();
+  console.log(`Document has ${docInfo.sheetCount} sheets.`);
+
+  // jira.on({ jiraEvent: "jira:issue_created" }, (event, app) => {
+  //   console.log(event.webhookEvent);
+  //   console.log(event.issue);
+  // });
 
   app.start(8000);
 })().catch(console.error);
